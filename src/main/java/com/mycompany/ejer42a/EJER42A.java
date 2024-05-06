87ece5b3dd98de0a0a9dc4242ae211ac0365f862
addycmm <164122301+addycmm@users.noreply.github.com>
2024-05-05 22:52:39 -0400

    TERMINADO

---
+++ src/main/java/com/mycompany/ejer42a/EJER42A.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejer42a;
+
+/**
+ *
+ * @author Leonel
+ */
+public class EJER42A {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
